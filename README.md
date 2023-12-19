1f4fa2b09b42624ccccb563fd3c9dedef33f62ed
rm0357 <r.morgunoff@gmail.com>
2023-12-20 00:30:39 +0100

    added more coins and time frames

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
+Version 0.0.3
+--------------
+Added multiple coins and multiple timeframes
+
+Version 0.0.2
+--------------
+Added possibility to get ohlcv data for a list of coins.
+
+
 VERSION 0.0.1
+--------------
 added basic grabbing of ohlcv data
 
 to start the project do `git clone` && run `docker-compose -f app.yaml up`
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io"
+	"log"
 	"net/http"
 
 	// "./handlers/handlers"
@@ -9,9 +10,9 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func getPrice(tf string) gin.HandlerFunc {
+func getPrice(tf string, coin string) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		url := "https://api-pub.bitfinex.com/v2/candles/trade:" + tf + ":tBTCUSD/hist" // 5m,30m,1h,1d 90req/min
+		url := "https://api-pub.bitfinex.com/v2/candles/trade:" + tf + ":" + coin + "/hist" // 5m,30m,1h,1d 90req/min tBTCUSD
 		req, err := http.NewRequest("GET", url, nil)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create request"})
@@ -40,13 +41,56 @@ func getPrice(tf string) gin.HandlerFunc {
 	}
 }
 
-func main() {
-	r := gin.Default()
-	r.Handle("GET", "/btc5", getPrice("5m"))
-	r.Handle("GET", "/btc30", getPrice("30m"))
-	r.Handle("GET", "/btc1h", getPrice("1h"))
-	r.Handle("GET", "/btc1d", getPrice("1D"))
+func getDataFromApi(tf string, coin string) string {
+	url := "https://api-pub.bitfinex.com/v2/candles/trade:" + tf + ":" + coin + "/hist" // 5m,30m,1h,1d 90req/min tBTCUSD
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal("ERROR")
+	}
 
+	req.Header.Add("accept", "application/json")
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal("ERROR")
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal("ERROR")
+	}
+	return string(body)
+}
+
+func getOHLCVData(tf []string, coin string) gin.HandlerFunc { // Proper go routines. wait group. // FOR each coin in the list, do for each time frame and save as own result
+	return func(c *gin.Context) {
+
+		var m5, m30, h1, D1 string
+
+		m5 = getDataFromApi(tf[0], coin)
+		m30 = getDataFromApi(tf[1], coin)
+		h1 = getDataFromApi(tf[2], coin)
+		D1 = getDataFromApi(tf[3], coin)
+
+		c.JSON(http.StatusOK, map[string]interface{}{
+			"m5":  m5,
+			"m30": m30,
+			"1h":  h1,
+			"1D":  D1,
+		})
+	}
+}
+
+func main() { // TODO:  add go routines and auto restart in timer in next patch. Also start saving into the database
+
+	r := gin.Default()
+	tfList := []string{"5m", "30m", "1h", "1D"}
+
+	r.Handle("GET", "/bitcoin", getOHLCVData(tfList, "tBTCUSD"))
+	r.Handle("GET", "/ethereum", getOHLCVData(tfList, "tETHUSD"))
+	r.Handle("GET", "/chainlink", getOHLCVData(tfList, "tLINK:USD"))
+	r.Handle("GET", "/cardano", getOHLCVData(tfList, "tADAUSD"))
+	r.Handle("GET", "/polkadot", getOHLCVData(tfList, "tDOTUSD"))
 	r.Run(":7777")
 
 }
